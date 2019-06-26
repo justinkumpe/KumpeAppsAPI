b17d60b1cdf79e6d426eea42dab098fadb1ab05f
Justin Kumpe <jakumpe@kumpes.com>
2019-06-25 19:34:41 -0500

    Initial Commit

---
+++ KumpeApps/KumpeApps.h
@@ -0,0 +1,19 @@
+//
+//  KumpeApps.h
+//  KumpeApps
+//
+//  Created by Justin Kumpe on 6/25/19.
+//  Copyright © 2019 Justin Kumpe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KumpeApps.
+FOUNDATION_EXPORT double KumpeAppsVersionNumber;
+
+//! Project version string for KumpeApps.
+FOUNDATION_EXPORT const unsigned char KumpeAppsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KumpeApps/PublicHeader.h>
+
+
